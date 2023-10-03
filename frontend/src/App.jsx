f54d34e8244bcf85f84f80e20d66f6905587529b
Jubril Bello <jbello@uoguelph.ca>
2023-10-03 06:54:11 -0400

    Create homepage layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import "./App.scss";
 import PhotoList from "./components/PhotoList";
-import TopicList from './components/TopicList';
-import TopNavigationBar from './components/TopNavigationBar';
-
-
+import TopicList from "./components/TopicList";
+import TopNavigationBar from "./components/TopNavigationBar";
+import HomeRoute from 'routes/HomeRoute';
 
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      {/* <TopicList /> */}
-      <PhotoList />
-      
+      <HomeRoute />
     </div>
   );
 };
-
 
 export default App;
