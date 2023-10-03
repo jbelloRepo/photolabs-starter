b86ddb082d39cae6811d319a34926c648305e010
Jubril Bello <jbello@uoguelph.ca>
2023-10-03 06:46:21 -0400

    Fix styling issue with nav bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import '../styles/TopNavigationBar.scss'
-import TopicList from './TopicList';
-import FavBadge from './FavBadge';
+import React from "react";
+import "../styles/TopNavigationBar.scss";
+import TopicList from "./TopicList";
+import FavBadge from "./FavBadge";
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      {
-      <TopicList /> }
-      <FavBadge isFavPhotoExist={true} />  {/* Assuming you have favorites. Adjust the prop as needed. */}
+      <TopicList />
+      <FavBadge />
     </div>
   );
-}
+};
 
 export default TopNavigation;
