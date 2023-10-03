526d88b5d35073d2cd11d03cb458c67396d9551c
Jubril Bello <jbello@uoguelph.ca>
2023-10-03 03:32:30 -0400

    Initial working files

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -56,10 +57,12 @@ const sampleDataForPhotoList = [
   },
 ];
 
-const PhotoList = () => {
+const PhotoList = (props) => {
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {props.photos.map((photo, id) => (
+        <PhotoListItem key={photo.id} {...photo} />
+      ))}
     </ul>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
 const sampleDataForPhotoListItem = {
   id: "1",
   location: {
@@ -14,8 +13,26 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return (
+    <section className="photo-list__item">
+      <div>
+        <img className="photo-list__image" src={props.imageSource} />
+      </div>
+      <div className="photo-list__user-details">
+        <div>
+          <img className="photo-list__user-profile" src={props.profile} />
+        </div>
+        <div className="photo-list__user-info">
+          {props.username}
+          <div className="photo-list__user-location">
+            {`${props.location.city}, ${props.location.country}`}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PhotoListItem;
